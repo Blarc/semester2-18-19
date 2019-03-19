0d306af6d86bfb854c0187615915ab90b881eb7b
Blarc <jakob.malezic@gmail.com>
2019-03-19 20:52:54 +0100

    OS_izziv_3 fixed

--- OS/izziv_3/skripta.sh
+++ OS/izziv_3/skripta.sh
@@ -5,7 +5,7 @@ cat <<EOF > ukazi.sh
 cut -d: -f1 /etc/passwd | sort | uniq | wc -l
 
 # D
-cut -d: -f3 /etc/passwd | sort -g | gawk '$1 > 99'
+cut -d: -f3 /etc/passwd | sort -g | gawk '\$1 > 99'
 
 # F
 cut -d: -f7 /etc/passwd | sort | uniq -c | sort -g
--- OS/izziv_3/ukazi.sh
+++ OS/izziv_3/ukazi.sh
@@ -2,7 +2,7 @@
 cut -d: -f1 /etc/passwd | sort | uniq | wc -l
 
 # D
-cut -d: -f3 /etc/passwd | sort -g | gawk ' > 99'
+cut -d: -f3 /etc/passwd | sort -g | gawk '$1 > 99'
 
 # F
 cut -d: -f7 /etc/passwd | sort | uniq -c | sort -g
