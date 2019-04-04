4e3c288dd7614167ab15cefc4997140d923156dc
Blarc <jakob.malezic@gmail.com>
2019-04-04 13:44:38 +0200

    dodal PPJ_vaje_5

--- PPJ/vaje_5/avltree.ml
+++ PPJ/vaje_5/avltree.ml
@@ -28,6 +28,97 @@ let t' =
 		leaf 8
 	)
 
-let toList = function
+let rec toList = function
 	| Empty -> []
-	| Node 
+	| Node (v, _, l, r) -> toList l @ [v] @ toList r
+
+let cmp x y =
+	match compare x y with
+		| 0 -> Equal
+		| r when r < 0 -> Less
+		| _ -> Greater
+
+let search x = function
+	| Empty -> false
+	| Node (y, _, l, r) ->
+		begin match cmp x y with
+			| Equal -> true
+			| Less -> search x l  
+			| Greater -> search x r
+		end
+
+let rotateLeft = function
+	| Node (x, _, a, Node (y, _, b, c)) ->
+		node (y, node(x, a, b), c)
+	| t -> t
+
+let rotateRight = function
+	| Node (y, _, Node (x, _, a, b), c) ->
+		node(x, a, node(y, b, c))
+	| t -> t
+
+let imbalance = function
+	| Empty -> 0
+	| Node (_, _, l, r) -> height l - height r
+
+let balance = function
+	| Empty -> Empty
+	| Nod (x, _, l, r) as t ->
+		begin match imbalance t with
+			| 2 ->
+				begin match imbalance l with
+					| -1 -> rotateRight (node (x, rotateLeft, l, r))
+					| _ -> rotateRight t
+				end
+			| -2 ->
+				begin match imbalance r with
+					| 1 -> rotateLeft (node (x, l, rotateRight r))
+					| _ -> rotateLeft t
+				end
+			| _ -> t
+		end
+
+
+let t'' = node (5, 
+		node (3, 
+			node(1, leaf 0, Empty), 
+			leaf 4
+		), 
+		leaf 8
+	)
+
+
+let rec add x = function
+	| Empty -> leaf x
+	| Node (y, _, l, r) ->
+		begin match cmp x y with
+			| Equal -> t
+			| Less -> balance(node (y, add x l, r))
+			| Greater -> balance(node (y, l, add x r)) 
+		end
+
+
+let rec remove x = function
+	| Empty -> Empty
+	| Node (y, _, l, r) ->
+		let rec removeSuccessor = function
+			| Empty -> failwith "impossible"
+			| Node (x, _, Empty, r) -> (r, x)
+			| Node (x, _, l, r) ->
+				let (l', y) = removeSuccessor l in 
+					(balance (node (x, l', r)), y)
+		in
+		match cmp x y with
+			| Less -> balance (node(y, remove x l, r))
+			| Greater -> balance (node(y, l, remove x r))
+			| Equal -> 
+				begin match (l, r) with
+					| (_, Empty) -> l
+					| (Empty, _) -> r
+					| _ -> let (r', y') = removeSuccessor r in
+						balance (node(y', l, r'))
+				end
+
+
+				
+	
