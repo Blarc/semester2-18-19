e5aa65bdbdec98fa34f9fe8b44f705af8def937e
Blarc <jakob.malezic@gmail.com>
2019-04-09 23:36:34 +0200

    OS izziv_5 in_progress

--- OS/izziv_5/myecho_b.c
+++ OS/izziv_5/myecho_b.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
-{
+{	
+	for (int i = 1; i < argc; i++) {
+		int j = 0;
+		while (argv[i][j] != '\0') {
+			printf("%c", argv[i][j]);
+			char* tmp = argv[i][j];
+			write(1, tmp, 2);
+			j++;
+		}
+	}
+	printf("\n");
 	
 }
